7eeb60e02a80c7cbae958e2df3eea7b3ff7c8f37
Olivier Bruchez <olivier@bruchez.org>
2017-05-13 21:38:27 +0200

    Add Scalastyle

--- src/main/scala/org/bruchez/olivier/Arguments.scala
+++ src/main/scala/org/bruchez/olivier/Arguments.scala
@@ -11,39 +11,6 @@ case class Arguments(srcPath: Path,
 
 object Arguments {
   def apply(args: Array[String]): Try[Arguments] = {
-    @annotation.tailrec
-    def fromArgs(args: List[String], arguments: Arguments): Try[Arguments] =
-      args match {
-        case Nil =>
-          Success(arguments)
-        case arg :: remainingArgs =>
-          (arg match {
-            case OutputFormatArgument if remainingArgs.nonEmpty =>
-              Format.formatFromString(remainingArgs.head) match {
-                case Success(format) =>
-                  Success((arguments.copy(outputFormat = format), remainingArgs.tail))
-                case Failure(_) =>
-                  Failure(new IllegalArgumentException(s"Unexpected format: ${remainingArgs.head}"))
-              }
-            case ConstantBitrateArgument if remainingArgs.nonEmpty =>
-              Success((arguments.copy(outputBitrate = Cbr(remainingArgs.head)), remainingArgs.tail))
-            case VariableBitrateArgument if remainingArgs.nonEmpty =>
-              Try(remainingArgs.head.toInt) match {
-                case Success(quality) =>
-                  Success((arguments.copy(outputBitrate = Vbr(quality)), remainingArgs.tail))
-                case Failure(_) =>
-                  Failure(new IllegalArgumentException(s"Unexpected quality: ${remainingArgs.head}"))
-              }
-            case _ =>
-              Failure(new IllegalArgumentException(s"Unexpected argument: $arg"))
-          }) match {
-            case Success((newArguments, argsLeftToParse)) =>
-              fromArgs(argsLeftToParse, newArguments)
-            case Failure(throwable) =>
-              Failure(throwable)
-          }
-      }
-
     if (args.length >= 2) {
       val defaultArguments = Arguments(
         srcPath = Paths.get(args(args.length - 2)),
@@ -52,15 +19,51 @@ object Arguments {
       val argumentsTry = fromArgs(args = args.slice(0, args.length - 2).toList, arguments = defaultArguments)
 
       argumentsTry flatMap { arguments =>
-        if (arguments.srcPath.toString == arguments.dstPath.toString)
+        if (arguments.srcPath.toString == arguments.dstPath.toString) {
           Failure(new IllegalArgumentException("Source and destination paths cannot be the same"))
-        else
+        } else {
           Success(arguments)
+        }
       }
     } else {
       Failure(new IllegalArgumentException("Source and destination paths missing"))
     }
   }
+
+  // scalastyle:off cyclomatic.complexity
+  @annotation.tailrec
+  private def fromArgs(args: List[String], arguments: Arguments): Try[Arguments] =
+    args match {
+      case Nil =>
+        Success(arguments)
+      case arg :: remainingArgs =>
+        (arg match {
+          case OutputFormatArgument if remainingArgs.nonEmpty =>
+            Format.formatFromString(remainingArgs.head) match {
+              case Success(format) =>
+                Success((arguments.copy(outputFormat = format), remainingArgs.tail))
+              case Failure(_) =>
+                Failure(new IllegalArgumentException(s"Unexpected format: ${remainingArgs.head}"))
+            }
+          case ConstantBitrateArgument if remainingArgs.nonEmpty =>
+            Success((arguments.copy(outputBitrate = Cbr(remainingArgs.head)), remainingArgs.tail))
+          case VariableBitrateArgument if remainingArgs.nonEmpty =>
+            Try(remainingArgs.head.toInt) match {
+              case Success(quality) =>
+                Success((arguments.copy(outputBitrate = Vbr(quality)), remainingArgs.tail))
+              case Failure(_) =>
+                Failure(new IllegalArgumentException(s"Unexpected quality: ${remainingArgs.head}"))
+            }
+          case _ =>
+            Failure(new IllegalArgumentException(s"Unexpected argument: $arg"))
+        }) match {
+          case Success((newArguments, argsLeftToParse)) =>
+            fromArgs(argsLeftToParse, newArguments)
+          case Failure(throwable) =>
+            Failure(throwable)
+        }
+    }
+  // scalastyle:on cyclomatic.complexity
 
   private val OutputFormatArgument = "-f"
   private val ConstantBitrateArgument = "-cbr"
--- src/main/scala/org/bruchez/olivier/Format.scala
+++ src/main/scala/org/bruchez/olivier/Format.scala
@@ -22,27 +22,29 @@ sealed trait Format {
 }
 
 case object Aac extends Format {
-  private val baseFfmpegArguments = Seq("-c:a", "libmp3lame")
+  private val BaseFfmpegArguments = Seq("-c:a", "libmp3lame")
+  private val DefaultQuality = 5
 
   override val name: String = "aac"
 
-  override def ffmpegArguments(bitrate: Bitrate): Seq[String] = baseFfmpegArguments ++ (bitrate match {
+  override def ffmpegArguments(bitrate: Bitrate): Seq[String] = BaseFfmpegArguments ++ (bitrate match {
     case Cbr(targetBitrate) => Seq("-b:a", targetBitrate)
     case Vbr(targetQuality) => Seq("-vbr", targetQuality.toString)
   })
 
-  override val defaultBitrate = Vbr(quality = 5)
+  override val defaultBitrate = Vbr(quality = DefaultQuality)
 }
 
 case object Mp3 extends Format {
-  private val baseFfmpegArguments = Seq("-c:a", "libfdk_aac")
+  private val BaseFfmpegArguments = Seq("-c:a", "libfdk_aac")
+  private val DefaultQuality = 2
 
   override val name: String = "mp3"
 
-  override def ffmpegArguments(bitrate: Bitrate): Seq[String] = baseFfmpegArguments ++ (bitrate match {
+  override def ffmpegArguments(bitrate: Bitrate): Seq[String] = BaseFfmpegArguments ++ (bitrate match {
     case Cbr(targetBitrate) => Seq("-b:a", targetBitrate)
     case Vbr(targetQuality) => Seq("-q:a", targetQuality.toString)
   })
 
-  override val defaultBitrate = Vbr(quality = 2)
+  override val defaultBitrate = Vbr(quality = DefaultQuality)
 }
