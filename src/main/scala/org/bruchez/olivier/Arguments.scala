bbdfeb0c22a8f001c9830deaea13caba1131aeb7
Olivier Bruchez <olivier@bruchez.org>
2017-05-20 15:24:20 +0200

    Add trash directory argument

--- src/main/scala/org/bruchez/olivier/Arguments.scala
+++ src/main/scala/org/bruchez/olivier/Arguments.scala
@@ -6,6 +6,7 @@ import scala.util._
 
 case class Arguments(srcPath: Path,
                      dstPath: Path,
+                     trashPath: Path,
                      inputExtensionsToConvert: Set[String] = Arguments.DefaultInputExtensionsToConvert,
                      outputFormat: Format = Aac,
                      outputBitrate: Bitrate = Aac.defaultBitrate) {
@@ -17,9 +18,11 @@ object Arguments {
 
   def apply(args: Array[String]): Try[Arguments] = {
     if (args.length >= 2) {
-      val defaultArguments = Arguments(
-        srcPath = Paths.get(args(args.length - 2)).toAbsolutePath,
-        dstPath = Paths.get(args(args.length - 1)).toAbsolutePath)
+      val srcPath = Paths.get(args(args.length - 2)).toAbsolutePath
+      val dstPath = Paths.get(args(args.length - 1)).toAbsolutePath
+      val trashPath = dstPath.resolve(".FlacToMp3Trash")
+
+      val defaultArguments = Arguments(srcPath = srcPath, dstPath = dstPath, trashPath = trashPath)
 
       val argumentsTry = fromArgs(args = args.slice(0, args.length - 2).toList, arguments = defaultArguments)
 
@@ -43,6 +46,8 @@ object Arguments {
         Success(arguments)
       case arg :: remainingArgs =>
         (arg match {
+          case TrashPathArgument if remainingArgs.nonEmpty =>
+            Success((arguments.copy(trashPath = Paths.get(remainingArgs.head)), remainingArgs.tail))
           case InputExtensionsToConvertArgument if remainingArgs.nonEmpty =>
             val extensions = remainingArgs.head.split(",").map(_.trim.toLowerCase).toSet
             Success((arguments.copy(inputExtensionsToConvert = extensions), remainingArgs.tail))
@@ -73,6 +78,7 @@ object Arguments {
     }
   // scalastyle:on cyclomatic.complexity
 
+  private val TrashPathArgument = "-trash"
   private val InputExtensionsToConvertArgument = "-ext"
   private val OutputFormatArgument = "-f"
   private val ConstantBitrateArgument = "-cbr"
