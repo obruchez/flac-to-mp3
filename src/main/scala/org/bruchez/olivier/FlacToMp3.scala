e7909ef517852e35ecd92d06ee97396bc825f225
Olivier Bruchez <olivier@bruchez.org>
2017-05-20 17:00:15 +0200

    Add some of the main logic (take input/output paths and produce actions to execute)

--- src/main/scala/org/bruchez/olivier/FlacToMp3.scala
+++ src/main/scala/org/bruchez/olivier/FlacToMp3.scala
@@ -1,5 +1,7 @@
 package org.bruchez.olivier
 
+import java.nio.file._
+
 import scala.util._
 
 object FlacToMp3 {
@@ -12,8 +14,48 @@ object FlacToMp3 {
         System.err.println(throwable.getMessage)
         System.exit(-1)
       case Success(arguments) =>
-        println(s"arguments = $arguments")
-        // @todo
+        convert(arguments)
     }
+  }
+
+  def convert(arguments: Arguments): Unit = {
+    println("Parsing source files...")
+    val srcPaths = FileUtils.allFilesInPath(arguments.srcPath).filterNot(FileUtils.osMetadataFile)
+    println(s"Source file count: ${srcPaths.size}")
+
+    println("Parsing destination files...")
+    val dstPaths = FileUtils.allFilesInPath(arguments.dstPath)
+    println(s"Destination file count: ${dstPaths.size}")
+
+    val actionGroups = this.actionGroups(srcPaths, dstPaths)
+
+    for (actionGroup <- actionGroups) {
+      Action.executeActions(actionGroup.actions)(arguments)
+    }
+  }
+
+  /*
+  File comparison:
+   - take source files and generate expected list of destination files (.flac -> .m4a, .mpp -> .m4a, etc.)
+   - compare expected destination files and actual destination files => generate actions
+   */
+
+  def actionGroups(srcPaths: Seq[Path], dstPaths: Seq[Path]): Seq[ActionGroup] = {
+    val removeSymbolicLinkActions = dstPaths.filter(Files.isSymbolicLink).map(RemoveSymbolicLinkAction)
+
+    // @todo
+    val removeFileActions = Seq[Action]()
+
+    // @todo
+    val convertFileActions = Seq[Action]()
+
+    // @todo
+    val copyFileActions = Seq[Action]()
+
+    Seq(
+      ActionGroup(removeSymbolicLinkActions),
+      ActionGroup(removeFileActions),
+      ActionGroup(convertFileActions),
+      ActionGroup(copyFileActions))
   }
 }
